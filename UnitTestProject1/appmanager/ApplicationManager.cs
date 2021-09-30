8625e7fe8f67af9013439b9fc7ef4a445aae100e
Maple <ellada-dger@mail.ru>
2021-09-30 14:58:59 +0300

    Optimization and login/logout tests

--- UnitTestProject1/appmanager/ApplicationManager.cs
+++ UnitTestProject1/appmanager/ApplicationManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using NUnit.Framework;
 using OpenQA.Selenium;
@@ -20,10 +21,12 @@ namespace WebAddressbookTests
         protected GroupHelper groupHelper;
         protected ContactHelper contactHelper;
 
-        public ApplicationManager ()
+        private static ThreadLocal<ApplicationManager> app = new ThreadLocal<ApplicationManager>();
+
+        private ApplicationManager ()
         {
             driver = new FirefoxDriver();
-            baseURL = "http://localhost/addressbook";
+            baseURL = "http://localhost";
 
             loginHelper = new LoginHelper(this);
             navigator = new NavigationHelper(this, baseURL);
@@ -31,15 +34,8 @@ namespace WebAddressbookTests
             contactHelper = new ContactHelper(this);
 
         }
-        public IWebDriver Driver
-        {
-            get
-            {
-                return driver;
-            }
-        }
 
-        public void Stop ()
+         ~ApplicationManager()
         {
             try
             {
@@ -50,6 +46,24 @@ namespace WebAddressbookTests
                 // Ignore errors if unable to close the browser
             }
 
+        }
+
+        public static ApplicationManager GetInstance ()
+        {
+            if (! app.IsValueCreated)
+            {
+                ApplicationManager newInstance = new ApplicationManager();
+                newInstance.Navigator.OpenHomePage();
+                app.Value = newInstance;
+            }
+            return app.Value;
+        }
+        public IWebDriver Driver
+        {
+            get
+            {
+                return driver;
+            }
         }
 
         public LoginHelper Auth
